8f094e6adb15983c4724ef2d5c9d21e15a8e4f79
Pely001 <pelypedrohenrique1@gmail.com>
2023-09-12 17:19:50 -0300

    modificado de println para print nas linhas 7 e 9

--- src/Contadores/Contador.java
+++ src/Contadores/Contador.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
+        System.out.print("Digite o primeiro parâmetro");
         int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
+        System.out.print("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
         
         try {
